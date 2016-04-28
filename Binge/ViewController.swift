546335825460a83ffbca94e8dcb974d25a15d736
Prashant Sinha <prashant@ducic.ac.in>
2016-04-28 23:31:29 +0530

    Initial Commit

---
+++ Binge/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Binge
+//
+//  Created by Prashant Sinha on 28/04/2016.
+//  Copyright © 2016 Prashant Sinha. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
