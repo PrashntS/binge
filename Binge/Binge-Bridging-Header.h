7b46f6dc6c1ab808f37b7fa91b3b875ae3f977ac
Prashant Sinha <prashant@ducic.ac.in>
2016-04-29 13:06:48 +0530

    + ext

---
+++ Binge/Binge-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Binge-Bridging-Header.h
+//  Binge
+//
+//  Created by Prashant Sinha on 29/04/2016.
+//  Copyright © 2016 Prashant Sinha. All rights reserved.
+//
+
+#ifndef Binge_Bridging_Header_h
+#define Binge_Bridging_Header_h
+
+#import "WKWebViewPrivate.h"
+
+#endif /* Binge_Bridging_Header_h */
--- Binge/ViewController.swift
+++ Binge/ViewController.swift
@@ -7,11 +7,28 @@
 //
 
 import Cocoa
+import WebKit
 
-class ViewController: NSViewController {
+
+class WebViewController: NSViewController, WKNavigationDelegate {
+    var webView = WKWebView()
+    let netflixURL = NSURL(string: "https://google.com")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addTrackingRect(view.bounds, owner: self, userData: nil, assumeInside: false)
+        
+        view.addSubview(webView)
+        webView.frame = view.bounds
+        webView.autoresizingMask = [NSAutoresizingMaskOptions.ViewHeightSizable, NSAutoresizingMaskOptions.ViewWidthSizable]
+        webView.configuration.preferences.plugInsEnabled = true
+        webView._customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12"
+        webView.allowsMagnification = true
+        
+        // Alow back and forth
+        webView.allowsBackForwardNavigationGestures = true
+        
+        webView.loadRequest(NSURLRequest(URL: netflixURL!))
 
         // Do any additional setup after loading the view.
     }
@@ -21,7 +38,6 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
 
 }
 
